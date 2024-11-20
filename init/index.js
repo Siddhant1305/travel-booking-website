462d23cbbd651de986b9212d15d69f2e506d279e
Siddhant Pandey <siddhantpandey1305@gmail.com>
2024-11-20 23:34:33 +0530

    features3.0

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({ ...obj, owner: "672722e8e9ec3cb67e562bbb" }))
   await Listing.insertMany(initData.data);
   console.log("Data was Initialized");
 };
